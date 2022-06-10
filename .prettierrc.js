d908569cefae80c66dbecd09b2a37272e9e77112
enestasdemir <enestdr@gmail.com>
2022-06-10 21:58:28 +0300

    chore: Update prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
+  printWidth: 120,
+  jsxBracketSameLine: false,
   singleQuote: true,
-  trailingComma: 'all',
+  tabWidth: 2,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  trailingComma: 'none'
 };
