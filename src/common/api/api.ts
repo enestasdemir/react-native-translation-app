dca8326a8042d59f9140eefd8dae26396b3e4f77
enestasdemir <enestdr@gmail.com>
2022-06-11 13:50:05 +0300

    feat/api: Implement tranlsation API

---
+++ src/common/api/api.ts
@@ -0,0 +1,30 @@
+import { Language } from '../constants/enums';
+const axios = require('axios');
+
+export const translateText = async (language: Language, targetLanguage: Language, text: string) => {
+  const body = {
+    source: language,
+    target: targetLanguage,
+    q: text
+  };
+
+  const options = {
+    method: 'POST',
+    url: 'https://deep-translate1.p.rapidapi.com/language/translate/v2',
+    headers: {
+      'content-type': 'application/json',
+      'X-RapidAPI-Key': 'IxlqM6A2vHmshULjdgyqtfo8BsvUp1MA6Kwjsnw7hy896Y1KXK',
+      'X-RapidAPI-Host': 'deep-translate1.p.rapidapi.com'
+    },
+    data: body
+  };
+
+  return axios
+    .request(options)
+    .then(function (response: any) {
+      return response.data.data.translations.translatedText;
+    })
+    .catch(function (error: any) {
+      return error;
+    });
+};
